08f9d205d34c461fdb4d16ad6e6142f836da8d72
Carolina Tartaroti Shishito <24.00599-0@maua.br>
2025-06-11 03:27:20 -0300

    arrumando rodapé e fazendo frontend de pagina_jogador

--- scripts/pagina_jogador.js
+++ scripts/pagina_jogador.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const divAvt = document.querySelector(".circle");
     const divNome = document.querySelector("#NomeUsuario");
+    const divNickname = document.querySelector('#nicknameUsuario');
+    const divEmail = document.querySelector('#emailUsuario');
     const divTime = document.querySelector("#Jogo");
     const divHoras = document.querySelector("#HPaes");
 
     divAvt.innerHTML = localStorage.getItem("avatar");
     const dados = await buscarDadosUsuario();
     divNome.innerHTML = `Nome: ${dados.NomeCompleto}`;
-    if (dados.time){
-        divTime.innerHTML = dados.time;
+    divNickname.innerHTML = `Nickname: ${dados.Nickname}`;
+    divEmail.innerHTML = `Email: ${dados.Email}`;
+    
+    if (dados.Time){
+        divTime.innerHTML = dados.Time;
     } else {
         divTime.innerHTML = "Não está em nenhum time específico."
     }
